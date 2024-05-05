08b8ee700f39a7a615259747974b7aafe1bd757b
Jason Kramer <jasonkramer7@gmail.com>
2024-05-05 15:17:24 -0500

    updated

--- src/app/videos/videos.component.ts
+++ src/app/videos/videos.component.ts
@@ -16,18 +16,42 @@ export class VideosComponent implements OnInit {
 
   constructor(){}
   ngOnInit(): void {
-    let dv1:DanceVideo = {
+    let dv:DanceVideo = {
+      title : "BDA 2018 1",
+      description: "Hannah in the Bellevue Dance Academy 2018 dance recital - Caterpillars Ball",
+      location: "assets/videos/BDA/BDA_2018_1.mp4"
+    };
+    this.videos.push(dv);
+    dv = {
+      title : "BDA 2018 2",
+      description: "Hannah in the Bellevue Dance Academy 2018 dance recital - Come Fly With Me",
+      location: "assets/videos/BDA/BDA_2018_2.mp4"
+    };
+    this.videos.push(dv);
+     dv = {
       title : "DDC Christmas 2022",
       description: "Hannah in the Dance Dynamics December 2022 dance recital - Silver Bells",
       location: "assets/videos/DanceDynamics/DDC_Christmas_2022.mov"
     };
-    this.videos.push(dv1);
-    let dv2: DanceVideo = {
+    this.videos.push(dv);
+    dv = {
       title : "DDC Spring 2023",
       description: "Hannah in the Dance Dynamics Spring 2023 dance recital - City of Stars",
       location: "assets/videos/DanceDynamics/DDC_2023.mov"
     };
-    this.videos.push(dv2);
+    this.videos.push(dv);
+    dv = {
+      title : "ODP 2024 Lyrical",
+      description: "Hannah in the Omaha Dance Project 2024 - What Was I Made For",
+      location: "assets/videos/ODP/ODP_2024_madefor.mp4"
+    };
+    this.videos.push(dv);
+    dv = {
+      title : "ODP 2024 Ballet",
+      description: "Hannah in the Omaha Dance Project - The Match Girl",
+      location: "assets/videos/ODP/ODP_2024_matchgirl.mp4"
+    };
+    this.videos.push(dv);
  
   }
 }
